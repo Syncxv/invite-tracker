0dca7282aaf2bfa1239283e550a7e861d5ba287f
Syncxv <aruldevarackal1@gmail.com>
2022-07-23 03:35:01 +0530

    idk man

---
+++ ui/src/pages/dashboard/servers/[id].tsx
@@ -0,0 +1,12 @@
+import Layout from '../../../components/Layout'
+import { FunctionalComponent } from '../../../types/react'
+
+const ManageServer: FunctionalComponent = ({}) => {
+    return (
+        <Layout>
+            <div>HEHEHE</div>
+        </Layout>
+    )
+}
+
+export default ManageServer
