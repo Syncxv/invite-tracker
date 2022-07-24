9b4f4d8e8876ff600710b657d6d2d0862be97c55
Syncxv <aruldevarackal1@gmail.com>
2022-07-24 17:17:25 +0530

    refactored it

--- bot/src/classes/ticketManager.ts
+++ bot/src/classes/ticketManager.ts
@@ -1,3 +1,57 @@
-export class TicketManager {}
+import {
+    GuildMember,
+    GuildMemberRoleManager,
+    Message,
+    MessageActionRow,
+    MessageButton,
+    CacheType,
+    ButtonInteraction
+} from 'discord.js'
+import { ButtonIds } from '../constants'
+import { GuildClass } from '../db/models/Guild'
+import { TicketModel } from '../db/models/Ticket'
 
-export const ticketManager = ((window as any).ticketManager = new TicketManager())
+export class TicketManager {
+    async claimTicket(interaction: ButtonInteraction<CacheType>) {
+        if (!interaction.isButton()) return
+        console.log(interaction)
+        if (!interaction.guild) return await interaction.reply({ content: 'bruh where tf is the guild wigga', ephemeral: true })
+        const guild = await GuildClass.getGuild(interaction.guild.id)
+        const ticket = await TicketModel.findOne({ channelId: interaction.channelId })
+        if (!guild.isSetUp() || ticket == null)
+            return await interaction.reply({
+                content:
+                    'bruh how did you even mnage to do this, SET UP YOU SERVER FIRST do /ticket setup-auto OOOOOR the ticket may not exist :|',
+                ephemeral: true
+            })
+        //check if they in the role list
+        if (
+            interaction.user.id !== interaction.guild.ownerId &&
+            !(interaction.member as GuildMember).permissions.has('ADMINISTRATOR') &&
+            !(interaction.member!.roles as GuildMemberRoleManager).cache.hasAny(...guild.ticketRoleIds)
+        )
+            return await interaction.reply({
+                content: 'only staff can do that :| if you a staff do `/ticket setup-add-roles` and add your role gg ez',
+                ephemeral: true
+            })
+        await ticket.updateOne({
+            $set: { responder: interaction.user.id }
+        })
+        await (interaction.message as Message).edit({
+            components: [
+                new MessageActionRow().addComponents(
+                    new MessageButton().setCustomId(ButtonIds.close).setLabel('Close').setStyle('DANGER').setEmoji('🔒'),
+
+                    new MessageButton()
+                        .setCustomId(ButtonIds.createCloseWithReason(ButtonIds.closeWithReason))
+                        .setLabel('Close With Reason')
+                        .setStyle('DANGER')
+                        .setEmoji('🔒')
+                )
+            ]
+        })
+        return await interaction.reply(`<@${interaction.user.id}> will handle yo ticket wigga boy`)
+    }
+}
+
+export const ticketManager = ((global as any).ticketManager = new TicketManager())
--- bot/src/index.ts
+++ bot/src/index.ts
@@ -1,16 +1,15 @@
 import { REST } from '@discordjs/rest'
 import { Routes } from 'discord-api-types/v9'
-import { Client, GuildMember, GuildMemberRoleManager, Intents, Message, MessageActionRow, MessageButton } from 'discord.js'
+import { Client, Intents } from 'discord.js'
 import dotenv from 'dotenv'
 import { apiMain } from './api'
 import inviteManager from './classes/inviteManager'
+import { ticketManager } from './classes/ticketManager'
 dotenv.config()
 
 import { getCommands } from './commands'
 import { ButtonIds } from './constants'
 import database from './db/db'
-import { GuildClass } from './db/models/Guild'
-import { TicketModel } from './db/models/Ticket'
 import { UserClass } from './db/models/User'
 import tryCatchExecute from './utils/tryCatch'
 
@@ -42,53 +41,10 @@ const main = async () => {
             }
         }
         if (interaction.isButton()) {
-            console.log(interaction)
             switch (interaction.customId) {
                 case ButtonIds.claim:
-                    if (!interaction.guild)
-                        return await interaction.reply({ content: 'bruh where tf is the guild wigga', ephemeral: true })
-                    const guild = await GuildClass.getGuild(interaction.guild.id)
-                    const ticket = await TicketModel.findOne({ channelId: interaction.channelId })
-                    if (!guild.isSetUp() || ticket == null)
-                        return await interaction.reply({
-                            content:
-                                'bruh how did you even mnage to do this, SET UP YOU SERVER FIRST do /ticket setup-auto OOOOOR the ticket may not exist :|',
-                            ephemeral: true
-                        })
-                    //check if they in the role list
-                    if (
-                        interaction.user.id !== interaction.guild.ownerId &&
-                        !(interaction.member as GuildMember).permissions.has('ADMINISTRATOR') &&
-                        !(interaction.member!.roles as GuildMemberRoleManager).cache.hasAny(...guild.ticketRoleIds)
-                    )
-                        return await interaction.reply({
-                            content:
-                                'only staff can do that :| if you a staff do `/ticket setup-add-roles` and add your role gg ez',
-                            ephemeral: true
-                        })
-                    await ticket.updateOne({
-                        $set: { responder: interaction.user.id }
-                    })
-                    await (interaction.message as Message).edit({
-                        components: [
-                            new MessageActionRow().addComponents(
-                                new MessageButton()
-                                    .setCustomId(ButtonIds.close)
-                                    .setLabel('Close')
-                                    .setStyle('DANGER')
-                                    .setEmoji('🔒'),
-
-                                new MessageButton()
-                                    .setCustomId(ButtonIds.createCloseWithReason(ButtonIds.closeWithReason))
-                                    .setLabel('Close With Reason')
-                                    .setStyle('DANGER')
-                                    .setEmoji('🔒')
-                            )
-                        ]
-                    })
-                    return await interaction.reply(`<@${interaction.user.id}> will handle yo ticket wigga boy`)
+                    return ticketManager.claimTicket(interaction)
             }
-            await interaction.reply({ content: ':)', ephemeral: true })
         }
     })
 
