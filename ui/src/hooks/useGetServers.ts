7e18ee78c900d00a25fa8c6b76e2a93d56b640b1
Syncxv <aruldevarackal1@gmail.com>
2022-12-06 17:57:09 +1100

    better

--- ui/src/hooks/useGetServers.ts
+++ ui/src/hooks/useGetServers.ts
@@ -1,17 +1,12 @@
-import { useSession } from 'next-auth/react'
-import { useRouter } from 'next/router'
 import { useState, useEffect } from 'react'
 import botApi from '../botApi'
 import { Guild } from '../types/discord'
+import { useAuthenticated } from './useAuthenticated'
 
 export const useGetServers = () => {
-    const { status } = useSession()
-    const router = useRouter()
+    useAuthenticated()
     const [servers, setServers] = useState<Guild[]>([])
     useEffect(() => {
-        if (status !== 'authenticated') {
-            router.push('/')
-        }
         botApi.getGuilds().then(res => setServers(res))
     }, [])
     return servers
