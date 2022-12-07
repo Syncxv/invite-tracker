9c81bf3beede6d03f7aa7cae140acb2660121c16
Syncxv <aruldevarackal1@gmail.com>
2022-12-07 16:05:48 +1100

    remove /ui

--- ui/src/components/atoms/Divider.tsx
+++
@@ -1,5 +0,0 @@
-interface Props {}
-
-const Divider: React.FC<Props> = () => <div className="h-[1px] w-full bg-gray-400 opacity-30" />
-
-export default Divider
--- ui/src/util/getAuthLink.ts
+++
@@ -1,9 +0,0 @@
-const scopes = ['guilds', 'guilds.members.read', 'identify'].join(' ')
-
-const AUTH_LINK = `https://discord.com/api/oauth2/authorize?client_id=${
-    process.env.NEXT_PUBLIC_CLIENT_ID
-}&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fauth%2Fcallback%2Fdiscord&response_type=code&scope=${encodeURI(
-    scopes
-)}`
-
-export { AUTH_LINK }
