05f7b910845141446bbbb0c58df200c2f20c0771
Syncxv <aruldevarackal1@gmail.com>
2022-12-14 01:17:54 +1100

    whoops didnt have svelte kit

---
+++ ui/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    import "../scss/_main.scss"
+</script>
+<svelte:head>
+    <title>Hey there</title>
+</svelte:head>
+
+<main>
+    <h1 class="heading">main page</h1>
+</main>
+
+
+<style lang="scss">
+    .heading {
+        font-size: 5rem;
+    }
+</style>
