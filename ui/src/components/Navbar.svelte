5910822743f8b2d4f0bf37b9cc2d74adcf173e02
Syncxv <aruldevarackal1@gmail.com>
2022-12-14 02:03:13 +1100

    hm

---
+++ ui/src/components/Navbar.svelte
@@ -0,0 +1,52 @@
+<nav class="navbar">
+    <div class="navbar__first">
+        <span class="title">I-T</span>
+        <li ><a class="nav-item"  href="/home">Home</a></li>
+        <li ><a class="nav-item" href="/dashboard">Dashboard</a></li>
+    </div>
+
+    <button class="navbar__login-btn">
+        Login
+    </button>
+</nav>
+
+<style lang="scss">
+    .navbar {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 1rem 2rem;
+        &__login-btn{
+            background-color: var(--brand);
+            padding: .5rem 2rem;
+            border: none;
+            border-radius: 6px;
+            color: white;
+            &:hover {
+                cursor: pointer;
+            }
+        }
+
+        &__first {
+            .title {
+                font-size: 2rem;
+                margin-right: 4rem;
+            }
+            display: flex;
+            align-items: center;
+            justify-content: start;
+            gap: 2rem;
+            & > li {
+                list-style: none;
+                .nav-item {
+                    text-decoration: none;
+                    color: white;
+                    font-size: 1.1rem;
+                    &:hover {
+                        text-decoration: underline;
+                    }
+                }
+            }
+        }
+    }
+</style>
--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,15 +1,57 @@
+<script>
+
+import Navbar from "../components/Navbar.svelte";
+
+</script>
 
 <svelte:head>
     <title>Hey there</title>
 </svelte:head>
 
 <main>
-    <h1 class="heading">main page</h1>
+    <Navbar />
+    <div class="wrapper">
+        <div class="hero">
+            <h1 class="heading">Cool Bot</h1>
+            <p class="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, reprehenderit.</p>
+        </div>
+
+        <a href="/dashboard" class="cta-button">Dashboard</a>
+    </div>
 </main>
 
-
-<style lang="scss">
-    .heading {
-        font-size: 5rem;
+<style lang="scss"> 
+    .wrapper {
+        display: grid;
+        place-content: center;
+        height: 80vh;
+        text-align: center;
+        justify-items: center;
+        .hero {
+            display: flex;
+            flex-direction: column;
+            gap: 2rem;
+            margin-bottom: 2rem;
+            .heading {
+                font-size: 6rem;
+            }
+            .desc {
+                font-size: 2rem;
+            }
+        }
+        .cta-button {
+            width: fit-content;
+            background-color: var(--brand);
+            padding: .8rem 1rem;
+            font-weight: bold;
+            font-size: 1.5rem;
+            border: none;
+            text-decoration: none;
+            border-radius: 6px;
+            color: white;
+            &:hover {
+                cursor: pointer;
+            }
+        }
     }
 </style>
