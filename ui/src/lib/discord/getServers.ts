19928fd89d567139e4a99cfcf885a8e62ad02152
Syncxv <aruldevarackal1@gmail.com>
2022-12-23 01:36:55 +1100

    get servers + /servesr/[id] and some improvements in /servers

--- ui/src/lib/discord/getServers.ts
+++ ui/src/lib/discord/getServers.ts
@@ -1,7 +1,7 @@
 import { BOT_API } from '.';
 import type { APIUser } from '../types';
-import type { APIGuild } from 'discord-api-types/v10';
-export const getServers = async (userId: string, accessToken: string): Promise<APIGuild[]> =>
+import type { Guild } from 'discord.js/typings';
+export const getServers = async (userId: string, accessToken: string): Promise<Guild[]> =>
 	await (
 		await fetch(`${BOT_API}/api/v1/guilds/getConnectedGuilds/${userId}`, {
 			headers: { Authorization: `Bearer ${accessToken}` }
--- ui/src/routes/servers/+page.svelte
+++ ui/src/routes/servers/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-	import Navbar from "../../components/Navbar.svelte";
+import Navbar from "../../components/Navbar.svelte";
 import type {
     PageData
 } from "./$types";
 
 export let data: PageData
+console.log(data)
 </script>
 
 <main>
@@ -14,6 +15,13 @@ export let data: PageData
             {#each data.guilds as guild}
             <article>
                 <div class="grid">
+                    <div class="guild_icon">
+                        {#if guild.icon == null}
+                            <h5>{guild.nameAcronym}</h5>
+                        {:else}
+                            <img src={`https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}`} alt="">
+                        {/if}
+                    </div>
                     <h3>{guild.name}</h3>
                     <a role="button" href={`/servers/${guild.id}`}>Manage Server</a>
                 </div>
@@ -24,22 +32,50 @@ export let data: PageData
 </main>
 
 <style lang="scss">
-    main {
-        .container {
-            padding: var(--spacing);
-            height: 100vh;
+main {
+    .container {
+        padding: var(--spacing);
+        height: 100vh;
+        align-items: center;
+        justify-content: center;
+        place-content: center;
+
+        article {
+            display: flex;
             align-items: center;
-            justify-content: center;
-            place-content: center;
             
-            article {
+
+
+            .grid {
                 display: flex;
+                justify-content: space-between;
+                width: 100%;
                 align-items: center;
-                .grid {
-                    width: 100%;
-                    align-items: center;;
+                .guild_icon {
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    background-color: var(--background);
+                    width: 15% !important;
+                    border-radius: 999px;
+                    width: 1rem;
+                    overflow: hidden;
+                    aspect-ratio: 1 / 1;
+                }
+                h5 {
+                    margin: 0;
+                }
+                h3 {
+                    font-size: 1rem;
+                }
+
+                a {
+                    width: fit-content;
+                    justify-self: end;
+                    padding: calc(var(--form-element-spacing-vertical) * 0.5) calc(var(--form-element-spacing-horizontal) * 0.5);
                 }
             }
         }
     }
+}
 </style>
