8354306f8f000f782a7d209a4c8251202d313583
Syncxv <aruldevarackal1@gmail.com>
2022-12-26 23:07:30 +1100

    cool

--- ui/src/routes/servers/+page.svelte
+++ ui/src/routes/servers/+page.svelte
@@ -11,15 +11,17 @@ export let data: PageData
         <div class="grid">
             {#each data.guilds as guild}
             <article>
-                <div class="grid">
-                    <div class="guild_icon">
-                        {#if guild.icon == null}
-                            <h5>{guild.accronym}</h5>
-                        {:else}
-                            <img src={`https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}`} alt="">
-                        {/if}
+                <div class="bruh">
+                    <div class="guild">
+                        <div class="guild_icon">
+                            {#if guild.icon == null}
+                                <h5>{guild.accronym}</h5>
+                            {:else}
+                                <img src={`https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}`} alt="">
+                            {/if}
+                        </div>
+                        <h3>{guild.name}</h3>
                     </div>
-                    <h3>{guild.name}</h3>
                     {#if guild.has_bot}
                         <a role="button" href={`/servers/${guild.id}`}>Manage Server</a>
                     {:else}
@@ -42,7 +44,7 @@ main {
         place-content: center;
         .grid {
             place-content: center;
-            grid-template-columns: repeat(auto-fit, minmax(22rem, 0.1fr))
+            grid-template-columns: repeat(auto-fit, minmax(26rem, 1.1fr))
         }
 
         article {
@@ -51,30 +53,35 @@ main {
             
 
 
-            .grid {
+            .bruh {
                 display: flex;
                 justify-content: space-between;
-                width: 100%;
                 align-items: center;
-                .guild_icon {
+                width: 100%;
+                .guild {    
                     display: flex;
                     align-items: center;
-                    justify-content: center;
-                    background-color: var(--background);
-                    width: 15% !important;
-                    border-radius: 999px;
-                    width: 1rem;
-                    overflow: hidden;
-                    aspect-ratio: 1 / 1;
-                }
-                h5 {
-                    margin: 0;
-                }
-                h3 {
-                    font-size: 1rem;
-                    max-width: 50%;
-                    justify-self: start;
-                    text-align: center
+                    justify-content: start;
+                    .guild_icon {
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                        background-color: var(--background);
+                        width: 4rem !important;
+                        border-radius: 999px;
+                        width: 1rem;
+                        overflow: hidden;
+                        aspect-ratio: 1 / 1;
+                    }
+                    h5 {
+                        margin: 0;
+                    }
+                    h3 {
+                        font-size: 1rem;
+                        justify-self: start;
+                        margin-left: 1.5rem;
+                        text-align: start
+                    }
                 }
 
                 a {
